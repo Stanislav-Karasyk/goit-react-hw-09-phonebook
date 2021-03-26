b6c33f12b3084fe52157df6853e5b1591713b5df
Stanislav-Karasyk <99.kara.99@gmail.com>
2021-03-26 16:59:25 +0200

    Updates

---
+++ static/js/homePage.b74575e5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[1],{85:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return c}));n(0);var o=n(1);function c(){return Object(o.jsx)("div",{children:Object(o.jsx)("h2",{children:"Home Page"})})}}}]);
+//# sourceMappingURL=homePage.b74575e5.chunk.js.map
